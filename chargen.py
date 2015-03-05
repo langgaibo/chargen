d16b9bbe695735799ddde48ccffff2b308e3b5d3
langgaibo <gabe.langton@gmail.com>
2015-03-04 19:19:42 -0800

    Heavy experimentation on chargen.py may need to revert. I'm so close!!!

--- chargen.py
+++ chargen.py
@@ -3,9 +3,14 @@
 #D&D simple character generator
 import hashmap
 from random import randint
+from sys import exit
+
+#Thu Mar  5 00:28:09 UTC 2015
+#eliminating "hashmap" function because I think i figured an ugly way to do
+#exactly what I want.
 
 stats = hashmap.new()
-prompt = '>>> '
+prompt = '>: '
 
 def start():
 	mainmenu()
@@ -21,6 +26,13 @@ def mainmenu():
 		print 'What the fuck are you talking about? Try again.'
 		start()
 
+#try to do this without globals
+def attlist():
+	attdict = {1: 'Strength', 2: 'Constitution', 3: 'Dexterity', 4: 'Wisdom', 
+	5: 'Intelligence', 6: 'Charisma'}
+	print 'debug %r' % attdict
+	return attdict
+
 def basestat():
 	baseroll = [randint(1,6) for i in range(0,4)]
 	print 'debug: baseroll = %r' % baseroll
@@ -29,13 +41,33 @@ def basestat():
 	print 'debug: baseroll - delroll = %r' % baseroll
 	basestat = sum(baseroll)
 	print 'debug: basestat = %r' % basestat
+	#Thu Mar  5 01:27:21 UTC 2015
+	#tried removing this "return" call and got "none" for bstats list values in starter()
 	return basestat
+
+def stats():
+	# I get to keep this beautiful list comprehension
+	statlist = [basestat() for i in range(0,6)]
+	return statlist
+
+'''
+def debug():
+	#attlist()
+	stats()
+	exit(0)
+
+'''
 
 def starter():
 	bstats = [basestat() for i in range(0,6)]
 	print 'debug: bstats built from iterating basestat() into a list returns:'
 	print bstats
+	print '\n\n\ndebug: just pulling from stats() to get the statlist first:'
+	cstats = stats()
+	print cstats
+	print '\n\n\n'
 	#bstats.reverse()
+'''
 	hashmap.set(stats, 'Str', bstats.pop())
 	hashmap.set(stats, 'Con', bstats.pop())
 	hashmap.set(stats, 'Dex', bstats.pop())
@@ -47,7 +79,8 @@ def starter():
 	print '\nnow I need to figure out how to enforce order on the dict'
 	print 'or pass it to a sorting function\n'
 	start()
-
+'''
 #def selectrace():
+
 
 start()
