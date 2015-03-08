418546979d971f061604aa74be64bca13ae7c92e
langgaibo <gabe.langton@gmail.com>
2015-03-07 20:07:21 -0800

    BAM! Massive update, we'll call this version 1.0 -Converted core stat roll and modifiers to a global variable. -Added racial bonuses. -Reworked the major zip function, replacing my handmade one with the built-in zip() -Lots of cleanup of functions, learned passing properly with the plus_stat() function. -Debugged for a lot of the day.

--- chargen.py
+++ chargen.py
@@ -2,11 +2,14 @@
 
 from random import randint
 from sys import exit
+import racestats
 
-print 'D&D simple character generator'
-print 'version 0.2 朗盖博 2015'
+print '\nD&D simple character generator'
+print 'version 0.3 朗盖博 2015\n'
 
 statlist = None
+modtotal = 0
+race_selected = 0
 prompt = '>: '
 
 def basestat():
@@ -21,6 +24,279 @@ def generate_stats():
 	statlist = None
 	statlist = [basestat() for i in range(0,6)]
 
+def show_stats():
+	judgement()
+	select_race()
+
+def select_race():
+	global race_selected
+	race_selected = 1
+	print 'Enter a number to select race and apply modifiers:'
+	choice = int(raw_input(prompt))
+	if choice == 1:
+		human()	
+	elif choice == 2:
+		aasimar()
+	elif choice == 3:
+		dragonborn()
+	elif choice == 4:
+		dwarfs()
+	elif choice == 5:
+		elfs()
+	elif choice == 6:
+		gnomes()
+	elif choice == 7:
+		halflings()
+	elif choice == 8:
+		half_elf()
+	elif choice == 9:
+		half_orc()
+	elif choice == 10:
+		tiefling()
+	elif choice == 11:
+		race_selected = 0
+		print '\n\n\n Rerolling!\n\n\n'
+		generate_stats()
+		show_stats()
+	elif choice == 99:
+		exit(0)
+	else:
+		print "What?"
+		select_race()
+	mainmenu()
+
+def stats_review():
+	a = att_words()
+	s = statlist
+	combined = zip(a,s)
+	for tup in combined:
+		temp = []
+		for val in tup:
+			temp.append(str(val))
+		print ' '.join(temp)
+
+def plus_stat(stat1, stat2, race):
+	global statlist
+
+	if stat1 == 'str':
+		statlist[0] = (statlist[0] + 1)
+	elif stat1 == 'con':
+		statlist[1] = (statlist[1] + 1)
+	elif stat1 == 'dex':
+		statlist[2] = (statlist[2] + 1)
+	elif stat1 == 'wis':
+		statlist[3] = (statlist[3] + 1)
+	elif stat1 == 'int':
+		statlist[4] = (statlist[4] + 1)
+	elif stat1 == 'cha':
+		statlist[5] = (statlist[5] + 1)
+	else:
+		print "What? error typing stat1. Going back.\n"
+		if race == 'Human':
+			statlist = [i - 1 for i in statlist]
+			human()
+		elif race == 'Half-elf':
+			statlist[5] = statlist[5] - 2
+			half_elf()
+		else:
+			print 'An error occured.'
+			exit(0)
+
+	if stat2 == 'str':
+		statlist[0] = (statlist[0] + 1)
+	elif stat2 == 'con':
+		statlist[1] = (statlist[1] + 1)
+	elif stat2 == 'dex':
+		statlist[2] = (statlist[2] + 1)
+	elif stat2 == 'wis':
+		statlist[3] = (statlist[3] + 1)
+	elif stat2 == 'int':
+		statlist[4] = (statlist[4] + 1)
+	elif stat2 == 'cha':
+		statlist[5] = (statlist[5] + 1)
+	else:
+		print "What? error typing stat2. Going back.\n"
+		if race == 'Human':
+			statlist = [i - 1 for i in statlist]
+			human()
+		elif race == 'Half-elf':
+			statlist[5] = statlist[5] - 2
+			half_elf()
+		else:
+			print 'An error occured.'
+			exit(0)
+
+	print '\nFINAL STATS for your %s:' % race
+	judgement()
+
+def human():
+	global statlist
+	print '\nFirst: humans get +1 across the board! Patriarchs!\n'
+	statlist = [i + 1 for i in statlist]
+	stats_review()
+	print '\nNow, type the abbrev. name of the first stat to +1 (i.e. "str"):'
+	stat1 = raw_input(prompt)
+	print 'and the second stat to +1:'
+	stat2 = raw_input(prompt)
+	race = 'Human'
+	plus_stat(stat1, stat2, race)
+
+def aasimar():
+	global statlist
+	print '\n+2 Cha, Aashole!'
+	statlist[5] = statlist[5] + 2
+	print '\nFINAL STATS for your Aasimar:'
+	judgement()
+
+def dragonborn():
+	global statlist
+	print '\n+2 Str, +1 Cha! Put on some lotion, scales!'
+	statlist[0] = statlist[0] + 2
+	statlist[5] = statlist[5] + 1
+	print '\nFINAL STATS for your Dragonborn:'
+	judgement()
+
+def dwarfs():
+	global statlist
+	print '\nFirst: +2 Constitution, beardo!\n'
+	statlist[1] = statlist[1] + 2
+	print 'Select a subrace!' 
+	print '"h" for Hill Dwarf, "m" for Mountain Dwarf, or "q" to quit.'
+	subrace = raw_input(prompt)
+	if subrace == 'h':
+		statlist[3] = statlist[3] + 1
+		race = 'Hill Dwarf'
+	elif subrace == 'm':
+		statlist[0] = statlist[0] + 2
+		race = 'Mountain Dwarf'
+	elif subrace == 'q':
+		exit(0)
+	else:
+		print '\nWhat? Try again, dickhead.'
+		statlist[1] = statlist[1] - 2
+		dwarfs()
+
+	print '\nFINAL STATS for your %r:' % race
+	judgement()
+
+def elfs():
+	global statlist
+	print '\nFirst: +2 Dex, pointy!\n'
+	statlist[2] = statlist[2] + 2
+	print 'Select a subrace!'
+	print '"h" for High Elf, "w" for Wood Elf,'
+	print '"d" for Drow, "e" for Eladrin, or "q" to quit.'
+	subrace = raw_input(prompt)
+	if subrace == 'h':
+		statlist[4] = statlist[4] + 1
+		race = 'High Elf'
+	elif subrace == 'w':
+		statlist[3] = statlist[3] + 1
+		race = 'Wood Elf'
+	elif subrace == 'd':
+		statlist[5] = statlist[5] + 1
+		race = 'Drow'
+	elif subrace == 'e':
+		statlist[4] = statlist[4] + 1
+		race = 'Eladrin'
+	elif subrace == 'q':
+		exit(0)
+	else:
+		print '\nWhat? Try again, dickhead.'
+		statlist[2] = statlist[2] - 2
+		elfs()
+
+	print '\nFINAL STATS for your %r:' % race
+	judgement()
+
+def gnomes():
+	global statlist
+	print '\nFirst: +2 Int, Gizmo!\n'
+	statlist[4] = statlist[4] + 2
+	print 'Select a subrace!'
+	print '"f" for Forest Gnome, "r" for Rock Gnome, or "q" to quit.'
+	subrace = raw_input(prompt)
+	if subrace == 'f':
+		statlist[2] = statlist[2] + 1
+		race = 'Forest Gnome'
+	elif subrace == 'r':
+		statlist[1] = statlist[1] + 1
+		race = 'Rock Gnome'
+	elif subrace == 'q':
+		exit(0)
+	else:
+		print '\nWhat? Try again, dickhead.'
+		statlist[4] = statlist[4] - 2
+		gnomes()
+
+	print '\nFINAL STATS for your %r:' % race
+	judgement()
+
+def halflings():
+	global statlist
+	print '\nFirst: +2 Dex, Samwise!\n'
+	statlist[2] = statlist[2] + 2
+	print 'Select a subrace!'
+	print '"l" for Lightfoot, "s" for Stout, or "q" to quit.'
+	subrace = raw_input(prompt)
+	if subrace == 'l':
+		statlist[5] = statlist[5] + 1
+		race = 'Lightfoot clan Halfling'
+	elif subrace == 's':
+		statlist[1] = statlist[1] + 1
+		race = 'Stout clan Halfling'
+	elif subrace == 'q':
+		exit(0)
+	else:
+		print '\nWhat? Try again, dickhead.'
+		statlist[2] = statlist[2] - 2
+		halflings()
+
+	print '\nFINAL STATS for your %r:' % race
+	judgement()
+
+def half_elf():
+	global statlist
+	print '\nFirst: +2 Cha, you sexy beast!\n'
+	statlist[5] = statlist[5] + 2
+	stats_review()
+	print '\nNow, type the abbrev. name of the first stat to +1 (i.e. "str"):'
+	stat1 = raw_input(prompt)
+	print 'and the second stat to +1:'
+	stat2 = raw_input(prompt)
+	race = 'Half-elf'
+	plus_stat(stat1, stat2, race)
+
+def	half_orc():
+	global statlist
+	print '+2 Str, +1 Con, you ugly motherfucker!\n'
+	statlist[0] = statlist[0] + 2
+	statlist[1] = statlist[1] + 1
+	print '\nFINAL STATS for your Half-orc:'
+	judgement()
+
+def tiefling():
+	global statlist
+	print '+1 Int, +2 Cha, Tinkerbell!\n'
+	statlist[4] = statlist[4] + 1
+	statlist[5] = statlist[5] + 2
+	print '\nFINAL STATS for your Tiefling:'
+	judgement()	
+
+def modlist():
+	stats = statlist
+	modlist = []
+	for stat in stats:
+		if stat == 9:
+			mod = -1
+			modlist.append(mod)
+		else:
+			mod = (stat - 10) / 2
+			modlist.append(mod)
+	global modtotal
+	modtotal = sum(modlist)
+	return modlist
+
 def att_words():
 	a_w = [
 		'Strength    ',
@@ -31,60 +307,6 @@ def att_words():
 		'Charisma    ']
 	return a_w
 
-def show_stats():
-	a = att_words()
-	global statlist
-	s = statlist
-	combined = zip(a,s)
-	for tup in combined:
-		temp = []
-		for val in tup:
-			temp.append(str(val))
-		print ' '.join(temp)
-	select_race()
-
-'''
-def human():
-	# +1 to all six ability scores
-	# +1 to any two ability scores of your choice
-
-def aasimar():
-	# +2 charisma
-
-def dragonborn():
-	# +2 Strength, +1 Charisma
-
-def dwarf():
-	# Hill Dwarf: +2 constitution, +1 wisdom
-	# Mountain Dwarf: +2 Strength, +2 Constitution
-
-def elf():
-	# High elf: +2 Dex, +1 Int
-	# Wood elf: +2 Dex, +1 Wis
-	# Dark elf: +2 Dex, +1 Cha
-	# Eladrin: +2 Dex, +1 Int
-
-def gnome():
-	# forest gnome: +1 Dex, +2 Int
-	# rock gome: +1 Con, +2 Int
-
-def halfling():
-	# lightfoot: +2 Dex, +1 Cha
-	# stout: +1 Con, +2 Dex
-
-def half-elf():
-	# +2 cha, +1 to any 2 scores
-
-def half-orc():
-	# +2 Str, +1 Con
-
-def tiefling():
-	# +1 int, +2 cha
-'''
-def select_race():
-	print 'it works!'
-	mainmenu()
-
 def mod_words():
 	m_w = []
 	for i in range(0,6):
@@ -92,64 +314,51 @@ def mod_words():
 		m_w.append(word.rjust(18, '-'))
 	return m_w
 
-def modlist():
-	global statlist
-	stats = statlist
-	modlist = []
-	for stat in stats:
-		if stat == 9:
-			mod = 1
-			modlist.append(mod)
-		else:
-			mod = (stat - 10) / 2
-			modlist.append(mod)
-	modtotal = sum(modlist)
-	return modlist, modtotal
-
 def zip_all():
-	global statlist
 	s = statlist
+	m = modlist()
 	a = att_words()
-	m, modtotal = modlist()
 	w = mod_words()
-	block = []
 	length = len(a)
-	for i in range(length):
-		block.append((a[i], s[i], w[i], m[i]))
-	return block, modtotal
+	block = zip(a,s,w,m)
+	return block
 
-def final():
-	block, modtotal = zip_all()
+def display_block():
+	block = zip_all()
 	for line in block:
 		temp = []
 		for chunk in line:
 			temp.append(str(chunk))
 		print ' '.join(temp)
-	return modtotal
 
 def judgement():
-	print ''
-	modtotal = final()
+	global race_selected
+	display_block()
 	print '\nTotal mods = %i' % modtotal
+	
 	if modtotal >=3 and modtotal <= 6:
-		print 'Decent rolls.\n'
+		print 'Decent stats.\n'
 	elif modtotal > 6:
-		print 'Great rolls!\n'
+		print 'Great stats!\n'
 	else:
-		print 'Shit rolls!\n'
-	global statlist
-	stype = type(statlist)
-	print 'debug - global statlist is %r and of %r' % (statlist, stype)
-	mainmenu()
+		print 'Shit stats!\n'
+	
+	if race_selected == 0:
+		racestats.novel()
+		select_race()
+	else:
+		race_selected = 0
+		print '\nStarting over!\n\n\n\n\n\n\n'
+		mainmenu()
 
 def mainmenu():
 	print 'Input "r" to roll base stats, or "q" to quit.'
 	choice = raw_input(prompt)
-	if 'r' in choice:
+	
+	if choice == 'r':
 		generate_stats()
 		show_stats()
-		#judgement()
-	elif 'q' in choice:
+	elif choice == 'q':
 		exit(0)
 	else:
 		print 'What the fuck are you talking about? Try again.'
---
+++ racestats.py
@@ -0,0 +1,34 @@
+# coding: utf8
+def novel():
+	print '''Now to select racial bonuses.
+1. Human:
+	+1 to all six stats
+	+1 to any two stats of your choice
+2. Aasimar:
+	+2 Cha
+3. Dragonborn:
+	+2 Str, +1 Cha
+4. Dwarf:
+	Hill Dwarf: +2 Con, +1 Wis
+	Mountain Dwarf: +2 Str, +2 Con
+5. Elf:
+	High elf: +2 Dex, +1 Int
+	Wood elf: +2 Dex, +1 Wis
+	Drow: +2 Dex, +1 Cha
+	Eladrin: +2 Dex, +1 Int
+6. Gnome:
+	forest gnome: +1 Dex, +2 Int
+	rock gome: +1 Con, +2 Int
+7. Halfling:
+	lightfoot: +2 Dex, +1 Cha
+	stout: +1 Con, +2 Dex
+8. Half-elf:
+	+2 Cha, +1 to any two stats of your choice
+9. Half-orc:
+	+2 Str, +1 Con
+10. Tiefling:
+	+1 Int, +2 Cha
+
+11. Reroll base stats!
+99. Quit!
+'''
